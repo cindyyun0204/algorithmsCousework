f18e3f9b6118b60a7b0eeff62b379a08c1d077b6
pingho <pingho@eduroam-int-dhcp-97-185-32.ucl.ac.uk>
2024-02-21 09:36:14 +0000

    Added random number generation for points

--- graham.py
+++ graham.py
@@ -2,45 +2,10 @@ import random
 import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import timeit
 from time import sleep
 
 def grahams_scan(points):
-    # def random_points(n):
-    #     return [(random.random(), random.random()) for i in range(n)]
-
-    def sort(points):
-        def compare_angles(p1, p2):
-            angle1 = math.atan2(p1[1] - p0[1], p1[0] - p0[0])
-            angle2 = math.atan2(p2[1] - p0[1], p2[0] - p0[0])
-            return angle1 < angle2
-        
-        def partition(arr, low, high):
-            pivot = arr[high]
-            i = low - 1
-            for j in range(low, high):
-                if compare_angles(arr[j], pivot):
-                    i += 1
-                    arr[i], arr[j] = arr[j], arr[i]
-            arr[i + 1], arr[high] = arr[high], arr[i + 1]
-            return i + 1
-        
-        def quicksort(arr, low, high):
-            if low < high:
-                pi = partition(arr, low, high)
-                quicksort(arr, low, pi - 1)
-                quicksort(arr, pi + 1, high)
-
-        if len(points) < 3:
-            return points
-        p0 = min(points, key=lambda p: p[0])
-        points.remove(p0)
-        quicksort(points, 0, len(points) - 1)
-        points.insert(0, p0)
-        return points
-
-    def cross_product(p1, p2, p3):
-        return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
-    
     sorted_points = sort(points)
     stack = []
     for p in sorted_points:
@@ -49,6 +14,42 @@ def grahams_scan(points):
         stack.append(p)
     stack.append(sorted_points[0]) # added the starting point again to close the convex hull
     return stack
+
+def random_points(n):
+        return [(random.randint(0, 1000), random.randint(0, 1000)) for i in range(n)]
+
+def sort(points):
+    def compare_angles(p1, p2):
+        angle1 = math.atan2(p1[1] - p0[1], p1[0] - p0[0])
+        angle2 = math.atan2(p2[1] - p0[1], p2[0] - p0[0])
+        return angle1 < angle2
+    
+    def partition(arr, low, high):
+        pivot = arr[high]
+        i = low - 1
+        for j in range(low, high):
+            if compare_angles(arr[j], pivot):
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
+    
+    def quicksort(arr, low, high):
+        if low < high:
+            pi = partition(arr, low, high)
+            quicksort(arr, low, pi - 1)
+            quicksort(arr, pi + 1, high)
+
+    if len(points) < 3:
+        return points
+    p0 = min(points, key=lambda p: p[0])
+    points.remove(p0)
+    quicksort(points, 0, len(points) - 1)
+    points.insert(0, p0)
+    return points
+
+def cross_product(p1, p2, p3):
+    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
 
 def plot_points(points, hull): # need to figure out where to put this function
     x = [p[0] for p in points]
@@ -59,12 +60,13 @@ def plot_points(points, hull): # need to figure out where to put this function
     plt.title("Graham's Scan")
     plt.xlabel("x-axis")
     plt.ylabel("y-axis")
-    plt.scatter(x, y, 25, color = "black")  # points are size 25
+    plt.scatter(x, y, 10, color = "black")
     plt.plot(hull_x, hull_y, color = "red")
     plt.show()
 
-points = [(0,0),(1,1),(3,2),(1,5),(0.5,5),(-1,3),(-0.5,2),(3,3.5),(8,-1)]
+# points = [(0,0),(1,1),(3,2),(1,5),(0.5,5),(-1,3),(points-0.5,2),(3,3.5),(8,-1)]
+    
+points = random_points(500)
 print(grahams_scan(points))
 hull = grahams_scan(points)
-
 plot_points(points, hull)
