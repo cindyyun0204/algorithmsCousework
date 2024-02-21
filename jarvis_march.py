465023e1ca0943ebdc33c50d1a1567433e742df5
pingho <pingho@eduroam-int-dhcp-97-185-32.ucl.ac.uk>
2024-02-21 10:36:47 +0000

    Added function to plot data and convex hull & function for random points

--- jarvis_march.py
+++ jarvis_march.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import random
+
 def left_most_index(points):
     first_index = 0
     for i in range(1, len(points)):
@@ -34,13 +37,32 @@ def jarvis_march(points,n):
         p = q
         if p == first:
             break
+    convex_hull.append(convex_hull[0]) # added the starting point again to close the convex hull
     return convex_hull
 
+def random_points(n):
+    return [(random.randint(0, 1000), random.randint(0, 1000)) for i in range(n)]
+
+def plot_points(points, hull):
+    x = [p[0] for p in points]
+    y = [p[1] for p in points]
+    hull_x = [p[0] for p in hull]
+    hull_y = [p[1] for p in hull]
+
+    plt.title("Jarvis March")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.scatter(x, y, 10, color = "black")
+    plt.plot(hull_x, hull_y, color = "red")
+    plt.show()
+
 def main():
-    points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
+    # points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
+    points = random_points(100)
     new = jarvis_march(points, len(points))
-    print(new)
-
-
+    new_points = [points[i] for i in new]
+    print(new_points)
+    plot_points(points, new_points)
+    
 if __name__ == "__main__":
     main()
