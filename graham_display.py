3c0b0178fdb26cdf670555c5eff4ab542ece5c8d
Ghostablisher <z2575569275@163.com>
2024-02-22 21:23:37 +0000

    update

--- graham_display.py
+++ graham_display.py
@@ -5,19 +5,45 @@ import matplotlib.animation as animation
 import timeit
 from time import sleep
 
-def graham_scan(points = None, randomize = True, plot = True):
+def graham_scan(points = [], plot = True):
     pts = []
 
     def random_points(n):
         for i in range(n):
-            pts.append((random.randint(0, 100), random.randint(0, 100)))
+            pts.append((random.randint(0, 32767), random.randint(0, 32767)))
         return pts
     
-    if randomize:
-        pts = random_points(100)
-    else:
-        pts = points
+    pts = points
+    if len(pts) == 0:
+        pts = random_points(1000000)
 
+    # def sort(pts):
+    #     def compare_angles(p1, p2):
+    #         angle1 = math.atan2(p1[1] - p0[1], p1[0] - p0[0])
+    #         angle2 = math.atan2(p2[1] - p0[1], p2[0] - p0[0])
+    #         return angle1 < angle2
+    #     def partition(arr, low, high):
+    #         pivot = arr[high]
+    #         i = low - 1
+    #         for j in range(low, high):
+    #             if compare_angles(arr[j], pivot):
+    #                 i += 1
+    #                 arr[i], arr[j] = arr[j], arr[i]
+    #         arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    #         return i + 1
+    #     def quicksort(arr, low, high):
+    #         if low < high:
+    #             pi = partition(arr, low, high)
+    #             quicksort(arr, low, pi - 1)
+    #             quicksort(arr, pi + 1, high)
+    #     if len(pts) < 3:
+    #         return pts
+    #     p0 = min(pts, key=lambda p: p[0])
+    #     pts.remove(p0)
+    #     quicksort(pts, 0, len(pts) - 1)
+    #     pts.insert(0, p0)
+    #     return pts
+        
     def sort(pts):
         def compare_angles(p1, p2):
             angle1 = math.atan2(p1[1] - p0[1], p1[0] - p0[0])
@@ -41,7 +67,7 @@ def graham_scan(points = None, randomize = True, plot = True):
             return pts
         p0 = min(pts, key=lambda p: p[0])
         pts.remove(p0)
-        quicksort(pts, 0, len(pts) - 1)
+        pts.sort(key=lambda p: math.atan2(p[1] - p0[1], p[0] - p0[0]))
         pts.insert(0, p0)
         return pts
     
@@ -77,3 +103,4 @@ def graham_scan(points = None, randomize = True, plot = True):
     return stack
 
 # graham_scan()
+# print(timeit.timeit(stmt = "graham_scan(plot=False)", setup = "from __main__ import graham_scan", number = 1))
