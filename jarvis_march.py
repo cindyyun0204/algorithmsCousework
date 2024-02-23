99c86594e78586b30005f2a7445e62cd566ffd7a
Ghostablisher <z2575569275@163.com>
2024-02-23 23:17:02 +0000

    update

--- jarvis_march.py
+++ jarvis_march.py
@@ -1,27 +1,57 @@
 import matplotlib.pyplot as plt
 import random
+import timeit
 
-def left_most_index(points):
-    first_index = 0
-    for i in range(1, len(points)):
-        if points[i][0] < points[first_index][0]:
-            first_index = i
-        elif points[i][0] == points[first_index][0]:
-            if points[i][1] > points[first_index][1]:
+def jarvis_march(pts = [], plot = True):
+
+    points = pts
+
+    def random_points(n):
+        for i in range(n):
+            points.append((random.randint(0, 32767), random.randint(0, 32767)))
+        return points
+    
+    if len(points) == 0:
+        points = random_points(1000000)
+
+    def left_most_index(points):
+        first_index = 0
+        for i in range(1, len(points)):
+            if points[i][0] < points[first_index][0]:
                 first_index = i
-    return first_index
+            elif points[i][0] == points[first_index][0]:
+                if points[i][1] > points[first_index][1]:
+                    first_index = i
+        return first_index
 
-def determinant_orientation(p, q, r):
-    val = (q[1] - p[1]) * (r[0] - p[0]) - (q[0] - p[0]) * (r[1] - p[1])
+    def determinant_orientation(p, q, r):
+        val = (q[1] - p[1]) * (r[0] - p[0]) - (q[0] - p[0]) * (r[1] - p[1])
 
-    if val == 0:
-        return 0
-    elif val > 0:
-        return 1
-    else:
-        return 2
+        if val == 0:
+            return 0
+        elif val > 0:
+            return 1
+        else:
+            return 2
+        
+    def random_points(n):
+        for i in range(n):
+            points.append((random.randint(0, 32767), random.randint(0, 32767)))
+        return points
+    
+    def plot_points(points, hull):
+        x = [p[0] for p in points]
+        y = [p[1] for p in points]
+        hull_x = [p[0] for p in hull]
+        hull_y = [p[1] for p in hull]
 
-def jarvis_march(points):
+        plt.title("Jarvis March")
+        plt.xlabel("x-axis")
+        plt.ylabel("y-axis")
+        plt.scatter(x, y, 10, color = "black")
+        plt.plot(hull_x, hull_y, color = "red")
+        plt.show()
+
     n = len(points)
     if n < 3:
         return
@@ -38,29 +68,10 @@ def jarvis_march(points):
         if p == first:
             break
     convex_hull.append(convex_hull[0]) # added the starting point again to close the convex hull
+    if plot:
+        plot_points(points, convex_hull)
     return convex_hull
 
-def random_points(n):
-    return [(random.randint(0, 1000), random.randint(0, 1000)) for i in range(n)]
+# jarvis_march()
+# print(timeit.timeit(stmt = "jarvis_march(plot = False)", setup="from __main__ import jarvis_march", number=1))
 
-def plot_points(points, hull):
-    x = [p[0] for p in points]
-    y = [p[1] for p in points]
-    hull_x = [p[0] for p in hull]
-    hull_y = [p[1] for p in hull]
-
-    plt.title("Jarvis March")
-    plt.xlabel("x-axis")
-    plt.ylabel("y-axis")
-    plt.scatter(x, y, 10, color = "black")
-    plt.plot(hull_x, hull_y, color = "red")
-    plt.show()
-
-def main():
-    # points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
-    points = random_points(100)
-    print(jarvis_march(points))
-    plot_points(points, jarvis_march(points))
-    
-if __name__ == "__main__":
-    main()
