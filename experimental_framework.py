70aa73b126e055bd8b05138c24bcb68328bc2112
Ghostablisher <z2575569275@163.com>
2024-02-28 17:06:42 +0000

    update

--- experimental_framework.py
+++ experimental_framework.py
@@ -28,36 +28,36 @@ class ExperimentalFramework:
 
         return chans_time, graham_time, jarvis_time
 
-    # def plot_results(self):
-    #     h_values_set = set(h for _, h, _, _, _ in self.results)
-    #     for h in h_values_set:
-    #         plt.figure(figsize=(10, 6))
-    #         n_values, chans_times, graham_times, jarvis_times = zip(*[(n, ct, gt, jt) for n, h_val, ct, gt, jt in self.results if h_val == h])
-    #         plt.plot(n_values, chans_times, label='Chan\'s Algorithm')
-    #         plt.plot(n_values, graham_times, label='Graham\'s Scan')
-    #         plt.plot(n_values, jarvis_times, label='Jarvis\' March')
-    #         plt.xlabel('Number of Points (n)')
-    #         plt.ylabel('Time (seconds)')
-    #         plt.title(f'h = {h}')
-    #         plt.legend()
-    #     plt.show()
-
     def plot_results(self):
-        n_values, h_values, chans_times, graham_times, jarvis_times = zip(*self.results)
-        x_values = [f'{n}({h})' for n, h in zip(n_values, h_values)]
-        # print(x_values, chans_times, graham_times, jarvis_times)
-        plt.figure(figsize=(10, 6))
-        plt.plot(x_values, chans_times, label='Chan\'s Algorithm')
-        plt.plot(x_values, graham_times, label='Graham\'s Scan')
-        plt.plot(x_values, jarvis_times, label='Jarvis\' March')
-        plt.xlabel('Number of Points (n) and Hull Points (h) in the form n(h)')
-        plt.ylabel('Time (seconds)')
-        plt.legend()
-        plt.xticks(rotation=90)
-        plt.tight_layout()
+        h_values_set = set(h for _, h, _, _, _ in self.results)
+        for h in h_values_set:
+            plt.figure(figsize=(10, 6))
+            n_values, chans_times, graham_times, jarvis_times = zip(*[(n, ct, gt, jt) for n, h_val, ct, gt, jt in self.results if h_val == h])
+            plt.plot(n_values, chans_times, label='Chan\'s Algorithm')
+            plt.plot(n_values, graham_times, label='Graham\'s Scan')
+            plt.plot(n_values, jarvis_times, label='Jarvis\' March')
+            plt.xlabel('Number of Points (n)')
+            plt.ylabel('Time (seconds)')
+            plt.title(f'h = {h}')
+            plt.legend()
         plt.show()
 
-framework = ExperimentalFramework([100,10000,20000,30000], [10,20,30,100], (0, 100), (0, 100))
+    # def plot_results(self):
+    #     n_values, h_values, chans_times, graham_times, jarvis_times = zip(*self.results)
+    #     x_values = [f'{n}({h})' for n, h in zip(n_values, h_values)]
+    #     # print(x_values, chans_times, graham_times, jarvis_times)
+    #     plt.figure(figsize=(10, 6))
+    #     plt.plot(x_values, chans_times, label='Chan\'s Algorithm')
+    #     plt.plot(x_values, graham_times, label='Graham\'s Scan')
+    #     plt.plot(x_values, jarvis_times, label='Jarvis\' March')
+    #     plt.xlabel('Number of Points (n) and Hull Points (h) in the form n(h)')
+    #     plt.ylabel('Time (seconds)')
+    #     plt.legend()
+    #     plt.xticks(rotation=90)
+    #     plt.tight_layout()
+    #     plt.show()
+
+framework = ExperimentalFramework([30000,50000,100000,110000,120000,130000,150000], [10,30,1000], (1, 32767), (1, 32767))
 # gonna change into range() eventually
 # n_range, h_range, x_range, y_range
 framework.run_experiment()
