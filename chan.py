d292598089fc319b1ad03fc473cccaafc0ec14cf
Ghostablisher <z2575569275@163.com>
2024-03-04 14:00:55 +0000

    update

--- chan.py
+++ chan.py
@@ -39,54 +39,58 @@ def chans_algorithm(points = [], plot = True):
     if len(pts) == 0:
         pts = random_points(1000000)
     n = len(pts)
-    m = int(math.sqrt(n))
-    subsets = [pts[i:i + m] for i in range(0, n, int(math.ceil(n/m)))]
-    hulls = [graham_scan(subset, plot=False) for subset in subsets]
-    p0 = min(min(hulls), key=lambda p: p[0])
-    final_hull = []
-    pthis = p0
-    if plot:
-        fig, ax = plt.subplots()
-    colors = [(random.random()/10, random.random()/10, random.random()) for _ in range(len(hulls))]
+    while m < n:
+        m = int(math.sqrt(n))
+        t = 1
+        subsets = [pts[i:i + m] for i in range(0, n, int(math.ceil(n/m)))]
+        hulls = [graham_scan(subset, plot=False) for subset in subsets]
+        p0 = min(min(hulls), key=lambda p: p[0])
+        final_hull = []
+        pthis = p0
+        if plot:
+            fig, ax = plt.subplots()
+        colors = [(random.random()/10, random.random()/10, random.random()) for _ in range(len(hulls))]
 
-    while True:
-    # for k in range(1, m+1):
-        if plot:
-            ax.clear()
-            ax.set_title("Chan's Algorithm")
-            ax.set_xlabel("x-axis")
-            ax.set_ylabel("y-axis")
-            ax.scatter(*zip(*pts), 10, color='black')
-            for i, hull in enumerate(hulls):
-                color = colors[i]
-                for j in range(len(hull)):
-                    ax.scatter(*hull[j], 10, color=color)
-                    ax.plot(*zip(*[hull[j-1], hull[j]]), color=color)
-                ax.plot(*zip(*[hull[-1], hull[0]]), color=color)
-        tangents = []
-        for hull in hulls:
-            tangent = search_tangent(hull, pthis)
-            tangents.append(tangent)
-        if plot:
-            ax.scatter(*zip(*tangents), 10, color='red')
-        most_cw_tangent = tangents[0]
-        for i in range(1, len(tangents)):
-            if orientation(pthis, most_cw_tangent, tangents[i]) < 0:
-                most_cw_tangent = tangents[i]
-        pthis = most_cw_tangent
-        final_hull.append(most_cw_tangent)
-        if plot:
-            ax.plot(*zip(*final_hull, final_hull[0]), color='green')
-            ax.scatter(*most_cw_tangent, 10, color='yellow')
-            for point in final_hull:
-                plt.scatter(*point, 10, color='green')
-            plt.pause(0.01)
-        if pthis == p0:
+        for _ in range(1, m+1):
+        # for k in range(1, m+1):
             if plot:
-                plt.tight_layout()
-                plt.show()
-            return final_hull
-    # return "incomplete"
+                ax.clear()
+                ax.set_title("Chan's Algorithm")
+                ax.set_xlabel("x-axis")
+                ax.set_ylabel("y-axis")
+                ax.scatter(*zip(*pts), 10, color='black')
+                for i, hull in enumerate(hulls):
+                    color = colors[i]
+                    for j in range(len(hull)):
+                        ax.scatter(*hull[j], 10, color=color)
+                        ax.plot(*zip(*[hull[j-1], hull[j]]), color=color)
+                    ax.plot(*zip(*[hull[-1], hull[0]]), color=color)
+            tangents = []
+            for hull in hulls:
+                tangent = search_tangent(hull, pthis)
+                tangents.append(tangent)
+            if plot:
+                ax.scatter(*zip(*tangents), 10, color='red')
+            most_cw_tangent = tangents[0]
+            for i in range(1, len(tangents)):
+                if orientation(pthis, most_cw_tangent, tangents[i]) < 0:
+                    most_cw_tangent = tangents[i]
+            pthis = most_cw_tangent
+            final_hull.append(most_cw_tangent)
+            if plot:
+                ax.plot(*zip(*final_hull, final_hull[0]), color='green')
+                ax.scatter(*most_cw_tangent, 10, color='yellow')
+                for point in final_hull:
+                    plt.scatter(*point, 10, color='green')
+                plt.pause(0.01)
+            if pthis == p0:
+                if plot:
+                    plt.tight_layout()
+                    plt.show()
+                return final_hull
+        t += 1
+        m = min(2**(2**t), len(pts))
+    return "incomplete"
 
 # print(len(chans_algorithm(plot=False)))
-# print(timeit.timeit(lambda: chans_algorithm(plot=False), number=1))
+print(timeit.timeit(lambda: chans_algorithm(plot=False), number=1))
