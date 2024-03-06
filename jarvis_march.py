c64d0e79ad99ccbb3a10acdc4d366b48a3e0c9f7
Monicazmh <149504792+Monicazmh@users.noreply.github.com>
2024-03-06 13:39:53 +0000

    add comment

--- jarvis_march.py
+++ jarvis_march.py
@@ -2,18 +2,11 @@ import matplotlib.pyplot as plt
 import random
 import timeit
 
-def jarvis_march(pts = [], plot = True):
-
+def jarvis_march(pts=[], plot=True):
+    # Initialize the list of points
     points = pts
 
-    def random_points(n):
-        for i in range(n):
-            points.append((random.randint(0, 32767), random.randint(0, 32767)))
-        return points
-    
-    if len(points) == 0:
-        points = random_points(1000000)
-
+    # Function to find the index of the leftmost point
     def left_most_index(points):
         first_index = 0
         for i in range(1, len(points)):
@@ -24,21 +17,18 @@ def jarvis_march(pts = [], plot = True):
                     first_index = i
         return first_index
 
+    # Function to determine the orientation of three points
     def determinant_orientation(p, q, r):
         val = (q[1] - p[1]) * (r[0] - p[0]) - (q[0] - p[0]) * (r[1] - p[1])
 
         if val == 0:
-            return 0
+            return 0  # Collinear
         elif val > 0:
-            return 1
+            return 1  # Clockwise
         else:
-            return 2
-        
-    def random_points(n):
-        for i in range(n):
-            points.append((random.randint(0, 32767), random.randint(0, 32767)))
-        return points
-    
+            return 2  # Counterclockwise
+
+    # Function to plot the points and the convex hull if required
     def plot_points(points, hull):
         x = [p[0] for p in points]
         y = [p[1] for p in points]
@@ -48,13 +38,16 @@ def jarvis_march(pts = [], plot = True):
         plt.title("Jarvis March")
         plt.xlabel("x-axis")
         plt.ylabel("y-axis")
-        plt.scatter(x, y, 10, color = "black")
-        plt.plot(hull_x, hull_y, color = "red")
+        plt.scatter(x, y, 10, color="black")
+        plt.plot(hull_x, hull_y, color="red")
         plt.show()
 
+    # Ensure there are at least 3 points to form a polygon
     n = len(points)
     if n < 3:
         return
+
+    # Start the Jarvis March algorithm
     first = left_most_index(points)
     convex_hull = []
     p = first
@@ -67,10 +60,16 @@ def jarvis_march(pts = [], plot = True):
         p = q
         if p == first:
             break
-    convex_hull.append(convex_hull[0]) # added the starting point again to close the convex hull
+
+    # Close the convex hull by adding the starting point again
+    convex_hull.append(convex_hull[0])
+
+    # Plot the points and the convex hull if plot is True
     if plot:
         plot_points(points, convex_hull)
+
     return convex_hull
+
 
 # jarvis_march()
 # print(timeit.timeit(stmt = "jarvis_march(plot = False)", setup="from __main__ import jarvis_march", number=1))
