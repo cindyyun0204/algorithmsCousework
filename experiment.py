cdefebacdc59a2652b70880d118887f31d766517
pingho <pingho@chicken-wing.home>
2024-03-06 03:34:12 +0000

    added 3d graph

--- experiment.py
+++ experiment.py
@@ -65,13 +65,13 @@ class ExperimentalFramework:
                 plt.plot(n_values, chans_times, label='Chan\'s Algorithm')
                 plt.scatter(n_values, chans_times, 5, color='black')
             if self.include_graham:
-                plt.plot(n_values, graham_times, label='Graham Scan')
+                plt.plot(n_values, graham_times, label='Graham Scan Algorithm')
                 plt.scatter(n_values, graham_times, 5, color='black')
             if self.include_jarvis:
-                plt.plot(n_values, jarvis_times, label='Jarvis March')
+                plt.plot(n_values, jarvis_times, label='Jarvis March Algorithm')
                 plt.scatter(n_values, jarvis_times, 5, color='black')
             plt.xlabel('Number of Points (n)')
-            plt.ylabel('Time (seconds)')
+            plt.ylabel('Execution Time (seconds)')
             plt.legend()
             if self.equal:
                 plt.title(f'n = h')
@@ -87,7 +87,7 @@ class ExperimentalFramework:
             if self.include_graham:
                 plt.plot(n_values, graham_times, label=str(h))
                 plt.scatter(n_values, graham_times, 5, color='black')
-                title = 'Graham Scan'
+                title = 'Graham Scan Algorithm'
             if self.include_chans:
                 plt.plot(n_values, chans_times, label=str(h))
                 plt.scatter(n_values, chans_times, 5, color='black')
@@ -95,29 +95,52 @@ class ExperimentalFramework:
             if self.include_jarvis:
                 plt.plot(n_values, jarvis_times, label=str(h))
                 plt.scatter(n_values, jarvis_times, 5, color='black')
-                title = 'Jarvis March'
+                title = 'Jarvis March Algorithm'
             plt.xlabel('Number of Points (n)')
-            plt.ylabel('Time (seconds)')
+            plt.ylabel('Execution Time (seconds)')
             plt.title(title)
             plt.legend()
         plt.show()  
 
     def plot_results_3d(self):
-        pass
+        n_values, h_values, chans_times, jarvis_times = zip(*[(n, h, ct, jt) for n, h, ct, _, jt in self.results])
+
+        if self.include_chans:
+            algorithm_time = chans_times
+            title = 'Chan\'s Algorithm'
+        if self.include_jarvis:
+            algorithm_time = jarvis_times
+            title = 'Jarvis March Algorithm'
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Color by execution time: normalize and map the execution time to a colormap
+        times_normalized = [t / max(algorithm_time) for t in algorithm_time]
+        colors = plt.cm.jet(times_normalized)
+
+        scatter = ax.scatter(n_values, h_values, algorithm_time, c=colors, marker='o')
+
+        ax.set_xlabel('Number of Points (n)')
+        ax.set_ylabel('Hull Size (h)')
+        ax.set_zlabel('Execution Time (seconds)')
+        plt.title(title)
+        plt.show()
 
     def print_results(self):
-        headers = ["n", "h", "Chan's Algorithm Time", "Graham Scan Time", "Jarvis March Time"]
+        headers = ['n', 'h', "Chan's Algorithm Time", 'Graham Scan Time', 'Jarvis March Time']
         data = self.results
         lengths = [max(len(str(val)) for val in col) for col in zip(headers, *data)]
         lengths = [max(len(headers[i]), lengths[i]) for i in range(len(lengths))]
-        row_format = "|".join(["{:<" + str(length) + "}" for length in lengths])
+        row_format = '|'.join(['{:<' + str(length) + '}' for length in lengths])
         print(row_format.format(*headers))
         for row in data:
             print(row_format.format(*row))
     
 n = range(10000,100001,10000)
 h = [3,10,25,100]
-framework = ExperimentalFramework(n, h, (0, 32767), (0, 32767)) # 3
+# framework = ExperimentalFramework(n, h, (0, 32767), (0, 32767)) # 3
+framework = ExperimentalFramework(range(1, 1000, 10), range(3, 100, 3), (0, 32767), (0, 32767), graham=False, jarvis=False)
 framework.run_experiment(count = 1)
-framework.plot_results()
+framework.plot_results_3d()
 # framework.print_results()
